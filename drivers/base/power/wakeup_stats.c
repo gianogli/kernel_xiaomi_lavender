ce501bdf72c5dab38a4eae142098d4e52e6b47e4
GIanluca <gianogli@tiscali.it>
2022-02-06 15:49:43 +0100

    test - adapt to 4.4

--- drivers/base/power/wakeup_stats.c
+++ drivers/base/power/wakeup_stats.c
@@ -39,8 +39,11 @@ static ssize_t active_time_ms_show(struct device *dev,
 				   struct device_attribute *attr, char *buf)
 {
 	struct wakeup_source *ws = dev_get_drvdata(dev);
-	ktime_t active_time =
-		ws->active ? ktime_sub(ktime_get(), ws->last_time) : 0;
+	ktime_t active_time;
+	if (ws->active)
+		active_time = ktime_sub(ktime_get(), ws->last_time);
+	else
+		active_time = ktime_set (0, 0);
 
 	return sprintf(buf, "%lld\n", ktime_to_ms(active_time));
 }
@@ -70,7 +73,7 @@ static ssize_t max_time_ms_show(struct device *dev,
 
 	if (ws->active) {
 		active_time = ktime_sub(ktime_get(), ws->last_time);
-		if (active_time > max_time)
+		if (ktime_after(active_time, max_time))
 			max_time = active_time;
 	}
 	return sprintf(buf, "%lld\n", ktime_to_ms(max_time));
